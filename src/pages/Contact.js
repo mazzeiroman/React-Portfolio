401fa088621c92e2f426f0139349b4bff7483e9d
Roman Mazzei <mazzeiroman@gmail.com>
2021-01-29 01:27:28 -0500

    contact page added

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,61 +1,120 @@
-import React, { Component } from "react";
-import API from "../utils/API";
+import React, { Component } from 'react';
+import "./style.css";
 import Container from "../components/Container";
-import SearchForm from "../components/SearchForm";
-import SearchResults from "../components/SearchResults";
-import Alert from "../components/Alert";
+import Row from "../components/Row";
+import Col from "../components/Col";
+import SocialMedia from "../components/SocialMedia";
 
 class Contact extends Component {
-  state = {
-    search: "",
-    breeds: [],
-    results: [],
-    error: ""
-  };
-
-  // When the component mounts, get a list of all available base breeds and update this.state.breeds
-  componentDidMount() {
-    API.getBaseBreedsList()
-      .then(res => this.setState({ breeds: res.data.message }))
-      .catch(err => console.log(err));
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      subject: '',
+      message: '',
+    };
   }
 
-  handleInputChange = event => {
-    this.setState({ search: event.target.value });
+  handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
   };
 
-  handleFormSubmit = event => {
-    event.preventDefault();
-    API.getDogsOfBreed(this.state.search)
-      .then(res => {
-        if (res.data.status === "error") {
-          throw new Error(res.data.message);
-        }
-        this.setState({ results: res.data.message, error: "" });
-      })
-      .catch(err => this.setState({ error: err.message }));
-  };
   render() {
     return (
       <div>
-        <Container style={{ minHeight: "80%" }}>
-          <h1 className="text-center">Search By Breed!</h1>
-          <Alert
-            type="danger"
-            style={{ opacity: this.state.error ? 1 : 0, marginBottom: 10 }}
-          >
-            {this.state.error}
-          </Alert>
-          <SearchForm
-            handleFormSubmit={this.handleFormSubmit}
-            handleInputChange={this.handleInputChange}
-            breeds={this.state.breeds}
-          />
-          <SearchResults results={this.state.results} />
+        <br></br>
+        <SocialMedia/>
+        <div>
+        <Container>
+        <br></br>
+        <div className="textarea"> 
+        <Row >
+          <Col size="md-12" >
+          <h1> Contact</h1>
+          </Col>
+        </Row>
+        </div>
+        <br></br>
+        </Container>
+        </div>
+        <Container>
+          <Row>
+          <Col size="md-12">
+            <br></br>
+      <div className='ContactForm textarea'>
+      <br></br>
+      <br></br>
+        <form id='contact-form' noValidate>
+          <div className='row'>
+            <div className='col-6'>
+              <input
+                type='text'
+                name='name'
+                value={this.state.name}
+                className='form-control formInput'
+                onChange={this.handleChange}
+                placeholder='Name'
+                noValidate
+              ></input>
+            </div>
+
+            <div className='col-6'>
+              <input
+                type='email'
+                name='email'
+                value={this.state.email}
+                className='form-control formInput'
+                onChange={this.handleChange}
+                placeholder='Email'
+                noValidate
+              ></input>
+            </div>
+          </div>
+
+          <div className='row'>
+            <div className='col-6'>
+              <input
+                type='subject'
+                name='subject'
+                value={this.state.subject}
+                className='form-control formInput'
+                onChange={this.handleChange}
+                placeholder='Subject'
+                noValidate
+              ></input>
+            </div>
+
+            <div className='col-6'>
+              <textarea
+                rows='5'
+                name='message'
+                value={this.state.message}
+                className='form-control formInput'
+                onChange={this.handleChange}
+                placeholder='Message'
+                noValidate
+              ></textarea>
+            </div>
+          </div>
+          <br></br>
+          <br></br>
+          <div className='title'>
+          <button className='submit-btn' type='submit'>
+            Submit
+          </button>
+          </div>
+        </form>
+        <br></br>
+        <br></br>
+      </div>
+      </Col>
+          </Row>
         </Container>
       </div>
     );
   }
 }
-
 export default Contact;
